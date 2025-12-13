b3dcf42e8b3bf9ecf38a34a02ee3ff6200187496
ganthantm65 <tmganthan@gmail.com>
2025-12-13 19:57:09 +0530

    6th

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "dist",
-    emptyOutDir: true
-  },
-  base: "/" 
+  plugins: [react(),
+    tailwindcss()
+  ]
 })
 
